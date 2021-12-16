c01644a2ec26c0f4a60eb10394e6a150d9e5c19d
JMI <ikolajm@gmail.com>
2021-12-16 10:19:34 -0500

    hid secret key generated by django

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!39$m12cc$s5k24b!0vg#jpy_$bfhozuuxyh5gcrtz#s39%x9o'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
